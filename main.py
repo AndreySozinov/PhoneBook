ae7b3925667fa8c60749746e2b443650d30101e4
AndreySozinov <savrey@ya.ru>
2022-05-25 07:14:55 +0500

    main module working

---
+++ main.py
@@ -0,0 +1,19 @@
+import UI
+import add_delete as ad
+import importbook
+import exportbook
+
+print('ADD - добавить запись\nDELETE - удалить запись\nEXPORT - экспортировать справочник\n\
+IMPORT - импортировать справочник')
+startword = input().lower
+
+if startword == "add":
+
+
+if startword == "delete":
+
+
+if startword == "export":
+
+
+if startword == "import":
